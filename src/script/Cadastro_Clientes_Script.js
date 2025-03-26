8b620a8661d6e55664ec50c000709d021e0b8a1c
Kaio Carvalho P <kaio.cp005@gmail.com>
2025-03-26 13:23:30 -0300

    parte do cadastro de clientes bonitas e funcinal só falta fazer com que os dados inseridos fiquem armazenados.

--- src/script/Cadastro_Clientes_Script.js
+++ src/script/Cadastro_Clientes_Script.js
@@ -1,129 +1,116 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("nome");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const input = document.getElementById("nome");
-    
-    function aplicarMascaraLetras(input) {
-    input.addEventListener("input", function() {
-        this.value = this.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, "");
+  function aplicarMascaraLetras(input) {
+    input.addEventListener("input", function () {
+      this.value = this.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, "");
     });
-}
+  }
 
-    const inputs = document.querySelectorAll('.form-control-letras');
-    inputs.forEach(aplicarMascaraLetras);
-
+  const inputs = document.querySelectorAll(".form-control-letras");
+  inputs.forEach(aplicarMascaraLetras);
 });
 
-// new Cleave('#telefone', {
-//     delimiters: ['(', ')', '-'],
-//     blocks: [0, 2, 5, 4],
-//     numericOnly: true
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const mostrarCPF = document.getElementById("cpf-radio");
+  const mostrarCNPJ = document.getElementById("cnpj-radio");
+  const cpf = document.getElementsByClassName("cpf")[0];
+  const cnpj = document.getElementsByClassName("cnpj")[0];
+  const cpfLabel = document.getElementById("cpf-label");
+  const cnpjLabel = document.getElementById("cnpj-label");
 
-// new Cleave('#cpf', {
-//     delimiters: ['.', '.', '-'],
-//     blocks: [3, 3, 3, 2],
-//     numericOnly: true
-// });
+  function adicionarCPF() {
+    if (mostrarCPF.checked) {
+      cpf.style.display = "block";
+      cnpj.style.display = "none";
+      cpfLabel.required = true;
+      cnpjLabel.required = false;
+    } else {
+      cpf.style.display = "none";
+    }
+  }
 
-// new Cleave('#cnpj', {
-//     delimiters: ['.', '.', '/', '-'],
-//     blocks: [2, 3, 3, 4, 2],
-//     numericOnly: true
-// });
+  function adicionarCNPJ() {
+    if (mostrarCNPJ.checked) {
+      cnpj.style.display = "block";
+      cpf.style.display = "none";
+      cnpjLabel.required = true;
+      cpfLabel.required = false;
+    } else {
+      cnpj.style.display = "none";
+    }
+  }
 
-// new Cleave('#cep', {
-//     delimiters: ['-'],
-//     blocks: [5, 3],
-//     numericOnly: true
-// });
-
-// new Cleave('#numero', {
-//     blocks: [5],
-//     numericOnly: true
-// });
-// new Cleave('#uf', {
-//     blocks: [2],
-//     uppercase: true,
-    
-// });
-
-function selecionaCEP(){
-    document.getElementById('rua')
-    document.getElementById('bairro')
-    document.getElementById('cidade')
-    document.getElementById('uf')   
-}
-
-function dadosPessoais(){
-    document.getElementById('nome')
-    document.getElementById('email')
-    document.getElementById('telefone')
-    document.getElementById('cpf')
-}
+  mostrarCPF.addEventListener("change", adicionarCPF);
+  mostrarCNPJ.addEventListener("change", adicionarCNPJ);
+});
 
 function limpa_formulario() {
-    document.getElementById('nome').value=("");
-    document.getElementById('email').value=("");
-    document.getElementById('telefone').value=("");
-    document.getElementById('cpf').value=("");
-    document.getElementById('rua').value=("");
-    document.getElementById('numero').value=("");
-    document.getElementById('bairro').value=("");
-    document.getElementById('cidade').value=("");
-    document.getElementById('uf').value=("");
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("telefone").value = "";
+  document.getElementById("cpf").value = "";
+  document.getElementById("rua").value = "";
+  document.getElementById("numero").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("uf").value = "";
 }
 
 function callback(conteudo) {
-    if (!("erro" in conteudo)) {
-        //Atualiza os campos com os valores.
-        document.getElementById('rua').value=(conteudo.logradouro);
-        document.getElementById('bairro').value=(conteudo.bairro);
-        document.getElementById('cidade').value=(conteudo.localidade);
-        document.getElementById('uf').value=(conteudo.uf);
-    } //end if.
-    else {
-        //CEP não Encontrado.
-        limpa_formulario();
-        alert("CEP não encontrado.");
-    }
+  if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById("rua").value = conteudo.logradouro;
+    document.getElementById("bairro").value = conteudo.bairro;
+    document.getElementById("cidade").value = conteudo.localidade;
+    document.getElementById("uf").value = conteudo.uf;
+  } //end if.
+  else {
+    //CEP não Encontrado.
+    limpa_formulario();
+    alert("CEP não encontrado.");
+  }
 }
 
-function pesquisacep(valor) {
+function pesquisacep(vrl) {
+  let valor = vrl.value;
+  var cep = valor.replace(/\D/g, "");
 
-    var cep = valor.replace(/\D/g, '');
+  if (cep.length == 8) {
+    console.log(cep.length);
 
     //Verifica se campo cep possui valor informado.
     if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
 
-        //Expressão regular para validar o CEP.
-        var validacep = /^[0-9]{8}$/;
-
-        //Valida o formato do CEP.
-        if(validacep.test(cep)) {
-
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
         //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('rua').value="...";
-        document.getElementById('bairro').value="...";
-        document.getElementById('cidade').value="...";
-        document.getElementById('uf').value="...";
+        document.getElementById("rua").value = "...";
+        document.getElementById("bairro").value = "...";
+        document.getElementById("cidade").value = "...";
+        document.getElementById("uf").value = "...";
 
         //Cria um elemento javascript.
-        var script = document.createElement('script');
+        var script = document.createElement("script");
 
         //Sincroniza com o callback.
-        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=callback';
+        script.src =
+          "https://viacep.com.br/ws/" + cep + "/json/?callback=callback";
         //Insere script no documento e carrega o conteúdo.
         document.body.appendChild(script);
-
-        } //end if.
-        else {
-                //cep é inválido.
-                limpa_formulario();
-                alert("Formato de CEP inválido.");
-        }
+        console.log(script);
+      } //end if.
+      else {
+        //cep é inválido.
+        limpa_formulario();
+        alert("Formato de CEP inválido.");
+      }
     } //end if.
     else {
-    //cep sem valor, limpa formulário.
-    limpa_formulario();
+      //cep sem valor, limpa formulário.
+      limpa_formulario();
     }
-};
+  }
+}
